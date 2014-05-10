7cd53d41c1d765f794c9ed7271ab0e27f2274480
suitougreentea <suitougreentea@users.noreply.github.com>
2014-05-10 11:35:23 +0900

    version bump (Milestone 3!)

--- src/io/github/suitougreentea/NeoBM/NeoBM.java
+++ src/io/github/suitougreentea/NeoBM/NeoBM.java
@@ -5,9 +5,9 @@ import java.util.logging.Logger;
 public class NeoBM {
     public static final Logger logger = Logger.getLogger("io.github.suitougreentea.NeoBM");
     public static final String NAME = "NeoBM";
-    public static final String MILESTONE = "M2";
-    public static final String MILESTONE_DESC = "Player Test";
-    public static final String VERSION = "0.2.4";
+    public static final String MILESTONE = "M3";
+    public static final String MILESTONE_DESC = "Gameplay Test";
+    public static final String VERSION = "0.3.0";
 
     public static String getFullVersion() {
         return String.format("%s %s (%s, %s)", NAME, MILESTONE, MILESTONE_DESC, VERSION);
